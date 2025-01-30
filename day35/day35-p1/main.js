37829153fa8b03468f38e382293efc0fa0cf5a28
joniys <132161817+joniys@users.noreply.github.com>
2025-01-30 20:41:18 +0100

    day35-p2

--- day35/day35-p1/main.js
+++ day35/day35-p1/main.js
@@ -1,26 +1,26 @@
 var slideinedx=1;
 
-function showslides(){
+function showslides(n){
     var i;
-    var slide = document.getElementById("slide");
+    var slide = document.getElementsByClassName("slide");
 
     for( i=0 ; i< slide.length; i++) {
-        slide=[i].style.display="none";
+        slide[i].style.display="none";
     }
-    if(n>slide.length){
+    if(n > slide.length){
         slideinedx=1;
     }
     if(n<1){
         slideinedx=slide.length;
     }
-    slide=[slideinedx].style.display="none";
+    slide[slideinedx].style.display="block";
 }
 
 showslides(1)
 
 function plusSlides(n){
     slideinedx += n;
-    showslides(1);
+    showslides(slideinedx);
 }
 
 setInterval(plusSlides, 3000,1)
