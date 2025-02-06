e9dbdfdffba554ce6b5ccfc5e3ace31d8a6d64d1
joniys <132161817+joniys@users.noreply.github.com>
2025-02-06 20:14:24 +0100

    day35

--- day36/main.js
+++ day36/main.js
@@ -17,10 +17,40 @@ $('#btn2').on("click",function() {
     console.log("sdasadsdsddasd")
 })
 
+
+
 $('#myDiv').click(function() {
     $(this).addClass("clicked");
 })
 
+.find('span')
+.attr('title','hovwe over me')
+
+
 $('#btn3').click(function(){
-    
+    $(".hidden").show('slow');
 })
+
+$('#btn4').click(function(){
+    $(".hidden").hide();
+})
+
+$('#square').click(function(){
+    $("#square").animate({
+    'width':'200px',
+    'height':'200px'
+})
+})
+$('#testing').click(function(){
+    $("#testing").animate({
+    'left':'+=50px',
+    'opacity': 0.25,
+    'fontSize':'30px'
+},300,
+    function(){
+        console.log("Animation is completed");
+    }
+)
+})
+
+
