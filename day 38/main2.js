4685a497114180227ade9da8ab93ca416b718e82
joniys <132161817+joniys@users.noreply.github.com>
2025-02-11 20:44:09 +0100

    day38

---
+++ day 38/main2.js
@@ -0,0 +1,23 @@
+(function () {
+    'use strict'
+    const forms = document.querySelectorAll('.requires-validation')
+    Array.from(forms)
+      .forEach(function (form) {
+        form.addEventListener('submit', function (event) {
+          if (!form.checkValidity()) {
+            event.preventDefault()
+            event.stopPropagation()
+          }
+    
+          form.classList.add('was-validated')
+        }, false)
+      })
+    })()
+
+    
+    function aaaa(){
+        const psd = document.getElementById("my").value;
+        console.log("bu5tton is clicked");
+        console.log(psd);
+
+    }
